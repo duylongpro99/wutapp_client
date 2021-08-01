9a9a0d0ac3412498659d117c20580680abb369e3
duylongpro99 <ripad3011@gmail.com>
2021-08-01 11:09:13 +0700

    config nav

--- pages/blogs/my-blog/index.vue
+++ pages/blogs/my-blog/index.vue
@@ -1,9 +1,22 @@
 <template src="./index.html"></template>
 <script lang="ts">
 import Vue from 'vue';
+import { mapMutations } from 'vuex';
 export default Vue.extend({
     data() {
         return {};
+    },
+    mounted() {
+        this.onMyBlogPage(true);
+    },
+
+    destroyed() {
+        this.onMyBlogPage(false);
+    },
+    methods: {
+        ...mapMutations({
+            onMyBlogPage: 'blogStore/onMyBlogPage',
+        }),
     },
 });
 </script>
